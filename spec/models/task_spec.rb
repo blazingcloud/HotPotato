6c9d94392f90ca5bce173dee4f6d26eae1e1935a
Curtis Schofield <o_o@Curtiss-MacBook-Pro.local>
2011-12-14 08:36:11 -0800

    Added check to make sure closed object is what we expect Added nil case
    Reworked spec to use 'let' block:
    
    this will create an accessor named 'memo_name' with the value of the
    block directly:
    
    let!(:memo_name) do
    end
    
    this will create a accessor named  'another_memo_name' with the value
    of the block the first time it is referenced:
    
    let(:another_memo_name) do
    end

--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -1,39 +1,45 @@
 require 'spec_helper'
 
 describe Task do
-
     it { should have_many(:activities) }
 
     it { should validate_presence_of(:person) }
 
-    it "should default to open" do
+    let(:my_person) do
+      my_person = Person.create!(:name => 'Kermit the Frog')
+    end
+    let(:my_task) do
       my_task = Task.create!(:name => 'Thing 1',
-                             :person => Person.create!)
+                             :person => my_person)
+    end
+
+    it "should default to open" do
       my_task.should_not be_closed
     end
 
     it "can be marked as closed" do
-      my_task = Task.create!(:name => 'Thing 1',
-                             :person => Person.create!)
       my_task.close
       my_task.should be_closed # closed? true
     end
 
     it "can't be closed twice" do
-      my_task = Task.create!(:name => 'Thing 1',
-                             :person => Person.create!)
       lambda do 
         my_task.close
         my_task.close
       end.should change(ClosedActivity,:count).by(1)
     end
 
-    it "should remember who closed the task" do
-      my_person = Person.create!(:name => 'Kermit the Frog')
-      my_task = Task.create!(:name => 'Thing 1',
-                             :person => my_person)
+    it "has no closed activity if it is not closed" do
+      my_task.closed.should be_nil
+    end
+
+    it "has a closed accessor which returns a ClosedActivity" do
+      my_task.closed.should be_instance_of ClosedActivity
+    end
+
+    it "sets task owner to be the initiator of the close activity" do
       my_task.close
-      my_task.closed.initiator_person.should be my_person
+      my_task.closed.initiator_person.should == my_task.person
     end
 
 end
